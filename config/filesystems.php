7bf9448f537eaae7af90b58623d8df9e6d857077
sweetsica <barondeepsnow@gmail.com>
2023-02-12 23:15:59 +0700

    add permission

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,6 +58,20 @@ return [
 
     ],
 
+    'local' => [
+        'driver' => 'local',
+        'root' => storage_path('app/public'),
+        'permissions' => [
+            'file' => [
+                'public' => 0644,
+                'private' => 0600,
+            ],
+            'dir' => [
+                'public' => 0755,
+                'private' => 0700,
+            ],
+        ],
+    ],
     /*
     |--------------------------------------------------------------------------
     | Symbolic Links
