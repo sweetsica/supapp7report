4eef6697410a21311ab6b4ce5265a86f27808ca2
sweetsica <barondeepsnow@gmail.com>
2023-02-13 00:05:30 +0700

    add permission

--- app/Http/Controllers/Api/ReportUploadController.php
+++ app/Http/Controllers/Api/ReportUploadController.php
@@ -43,7 +43,8 @@ class ReportUploadController extends Controller
         $file = $request->file('files');
         $name_file = $file->getClientOriginalName();
         $date = Carbon::today()->format('d-m-Y');
-        $path = Storage::putFileAs("public/report/".$date,$request->file('files'),$name_file);
+        $folder = Storage::makeDirectory("public/report/".$date);
+        $path = Storage::putFileAs($folder,$request->file('files'),$name_file);
         $link_file = URL::to('/').Storage::url('report/'.$date.'/'.$name_file);
 
         ReportUpload::create([
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::post('/test', function (){
+    \Illuminate\Support\Facades\Storage::makeDirectory('test');
+
+});
 Route::get('/report/getfile', [ReportUploadController::class, 'show']);
 Route::post('/report/upload',[ReportUploadController::class,'store']);
