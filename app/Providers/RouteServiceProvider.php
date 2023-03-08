bc30526aa64c51c0ff8a2b58460a9f342b186277
sweetsica <barondeepsnow@gmail.com>
2023-03-08 09:05:04 +0700

    fix route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,6 +26,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (str_contains(env('APP_URL'), 'https')) {
+            resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+        }
+
         $this->configureRateLimiting();
 
         $this->routes(function () {
